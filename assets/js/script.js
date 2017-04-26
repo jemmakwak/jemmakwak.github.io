8b4a1e5ee58f900ef42be4cd8e5ffa3f171133cf
Your Name <yeogyoungkwak@gmail.com>
2017-04-25 21:10:28 -0700

    @GITHUB STOP MIXING MY CONTENT

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,29 @@ $(document).ready(function(){
 
   // Scroll to Div
 
-  $(".about").click(function() {
+  $(".design").click(function() {
       $('html,body').animate({
-          scrollTop: $(".about").offset().top},
+          scrollTop: $(".worker").offset().top},
           'slow');
   });
-  $(".work").click(function() {
+  $(".exp").click(function() {
       $('html,body').animate({
-          scrollTop: $(".design").offset().top},
+          scrollTop: $(".experience-head").offset().top},
           'slow');
   });
   $(".contact").click(function() {
       $('html,body').animate({
-          scrollTop: $(".contact").offset().top},
+          scrollTop: $("#social-bar").offset().top},
           'slow');
+  });
+
+  $(".menu-item").hover(function(){
+    $(this).css("color", "#221F28");
+    $(this).css("-webkit-transition:color", ".2s ease-in")
+  });
+
+  $(".menu-item").mouseleave(function(){
+    $(this).css("color", "#95A5A6");
+    $(this).css("-webkit-transition:color", ".2s ease-out")
   });
 });
